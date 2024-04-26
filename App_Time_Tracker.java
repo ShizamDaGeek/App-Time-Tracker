3bbf4445ea3835065890a2d2cc8ce83ac5b95076
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-04-27 09:03:18 +1200

    Made a button

--- App_Time_Tracker.java
+++ App_Time_Tracker.java
@@ -6,16 +6,27 @@ public class App_Time_Tracker extends JFrame
     public App_Time_Tracker()
     {
         this.setTitle("App Time Tracker");
-        this.setSize(800, 600);
+        this.setSize(900, 900);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.getContentPane().setBackground(new Color(34, 32, 52));
+        this.setLayout(null);
+        this.setResizable(false);
+
+        // Buttons
+        JButton add_btn = new JButton();
+        add_btn.setText("Add");
+        add_btn.setBounds(0, 0, 100, 100);
+        add_btn.setBackground(Color.WHITE);
+        this.add(add_btn);
+
         this.setVisible(true);
     }
 
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println("This is a computer program made by ShizamDa_Geek");
         new App_Time_Tracker();
     }
 }
