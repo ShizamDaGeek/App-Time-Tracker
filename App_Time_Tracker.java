f4496f62e42a1718500c6874a9414dfec090b9dc
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-04-27 10:09:30 +1200

    made 2 button and added icons to them one is a settings button and the other is a add button to add what ever app to track

--- App_Time_Tracker.java
+++ App_Time_Tracker.java
@@ -1,7 +1,8 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class App_Time_Tracker extends JFrame
+public class App_Time_Tracker extends JFrame implements ActionListener 
 {
     public App_Time_Tracker()
     {
@@ -14,10 +15,14 @@ public class App_Time_Tracker extends JFrame
         this.setResizable(false);
 
         // Buttons
-        JButton add_btn = new JButton();
-        add_btn.setText("Add");
-        add_btn.setBounds(0, 0, 100, 100);
-        add_btn.setBackground(Color.WHITE);
+        ImageIcon setting_btn_Icon = new ImageIcon("icons/setting_btn_icon.png");
+        JButton setting_btn = new JButton(setting_btn_Icon);
+        setting_btn.setBounds(10, 10, 50, 50);
+        this.add(setting_btn);
+
+        ImageIcon add_btn_Icon = new ImageIcon("icons/add_btn_icon.png");
+        JButton add_btn = new JButton(add_btn_Icon);
+        add_btn.setBounds(70, 10, 50, 50);
         this.add(add_btn);
 
         this.setVisible(true);
@@ -28,5 +33,11 @@ public class App_Time_Tracker extends JFrame
         System.out.println("Hello World");
         System.out.println("This is a computer program made by ShizamDa_Geek");
         new App_Time_Tracker();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent btn_action) 
+    {
+
     }
 }
