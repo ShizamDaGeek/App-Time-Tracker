2865d281be0ab9712eee8e311f488bdef02c8b0a
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-04-28 06:49:43 +1200

    I added a popup dialog to the setting button and added a file popup as well but to the add button

--- App_Time_Tracker.java
+++ App_Time_Tracker.java
@@ -2,8 +2,11 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class App_Time_Tracker extends JFrame implements ActionListener 
+public class App_Time_Tracker extends JFrame implements ActionListener
 {
+    JButton setting_btn;
+    JButton add_btn;
+
     public App_Time_Tracker()
     {
         this.setTitle("App Time Tracker");
@@ -16,16 +19,42 @@ public class App_Time_Tracker extends JFrame implements ActionListener
 
         // Buttons
         ImageIcon setting_btn_Icon = new ImageIcon("icons/setting_btn_icon.png");
-        JButton setting_btn = new JButton(setting_btn_Icon);
+        setting_btn = new JButton(setting_btn_Icon);
         setting_btn.setBounds(10, 10, 50, 50);
+        setting_btn.addActionListener(this);
         this.add(setting_btn);
 
         ImageIcon add_btn_Icon = new ImageIcon("icons/add_btn_icon.png");
-        JButton add_btn = new JButton(add_btn_Icon);
+        add_btn = new JButton(add_btn_Icon);
         add_btn.setBounds(70, 10, 50, 50);
+        add_btn.addActionListener(this);
         this.add(add_btn);
 
         this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        if(e.getSource() == add_btn)
+        {
+            JFileChooser chooseApp = new JFileChooser();
+            chooseApp.showOpenDialog(null);
+        }
+
+        if(e.getSource() == setting_btn)
+        {
+            SettingsDialog();
+        }
+    }
+
+    private void SettingsDialog()
+    {
+        JDialog settingMenu = new JDialog(this, "Settings", true);
+        settingMenu.setTitle("Settings");
+        settingMenu.setBounds(600, 100, 400, 400);
+        settingMenu.setResizable(false);
+        settingMenu.setVisible(true);
     }
 
     public static void main(String[] args)
@@ -33,11 +62,5 @@ public class App_Time_Tracker extends JFrame implements ActionListener
         System.out.println("Hello World");
         System.out.println("This is a computer program made by ShizamDa_Geek");
         new App_Time_Tracker();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent btn_action) 
-    {
-
     }
 }
