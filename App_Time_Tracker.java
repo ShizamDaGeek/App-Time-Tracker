74e7af7688e71cc61ca671c2a1205253b3225e3b
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-05-02 09:18:38 +1200

    I put all the buttons in a panel which will alway be on the north side of the window

--- App_Time_Tracker.java
+++ App_Time_Tracker.java
@@ -16,29 +16,34 @@ public class App_Time_Tracker extends JFrame implements ActionListener
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.getContentPane().setBackground(new Color(34, 32, 52));
-        this.setLayout(null);
+        this.setLayout(new BorderLayout());
         this.setResizable(false);
+
+        JPanel topMenu = new JPanel();
+        topMenu.setBackground(Color.black);
+        topMenu.setPreferredSize(new Dimension(100, 100));
+        this.add(topMenu, BorderLayout.NORTH);
 
         // BUTTONS
         ImageIcon add_btn_Icon = new ImageIcon("icons/add_btn_icon.png");
         add_btn = new JButton(add_btn_Icon);
         add_btn.setBounds(10, 10, 50, 50);
         add_btn.addActionListener(this);
-        this.add(add_btn);
+        topMenu.add(add_btn);
 
         ImageIcon color_chooser_btn_Icon = new ImageIcon("icons/color_chooser_btn_icon.png");
         color_chooser_btn = new JButton(color_chooser_btn_Icon);
         color_chooser_btn.setBounds(70, 10, 50, 50);
         color_chooser_btn.addActionListener(this);
-        this.add(color_chooser_btn);
+        topMenu.add(color_chooser_btn);
 
         ImageIcon font_chooser_btn_Icon = new ImageIcon("icons/font_chooser_btn_icon.png");
         font_chooser_btn = new JButton(font_chooser_btn_Icon);
         font_chooser_btn.setBounds(130, 10, 50, 50);
         font_chooser_btn.addActionListener(this);
-        this.add(font_chooser_btn); 
+        topMenu.add(font_chooser_btn); 
 
-        // MAKING IT VISIBLE
+        // MAKING IT ALL VISIBLE
         this.setVisible(true);
     }
 
